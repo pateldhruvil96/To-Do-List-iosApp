6399beac27f1100a5a5a38ff1310b763b9e5892d
pateldhruvil96 <patel.dhruvil96@gmail.com>
2020-05-30 11:20:04 +0530

    Save Array of custom objects by creating own plist

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -2,15 +2,34 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    var itemArray=["kem cho","hoola","supp"]
-    let defaults = UserDefaults.standard //note that this is not database and for example to access the array then system has to open all the items in the plist. so this affects the efficient of the app.so it is recommended to use only to store small value and not to store large values.ie like storing more no. of values in array.
+    var itemArray=[Item]() //this turns array into array of Item objects
+    //let defaults = UserDefaults.standard //note that this is not database and for example to access the array then system has to open all the items in the plist. so this affects the efficient of the app.so it is recommended to use only to store small value and not to store large values.ie like storing more no. of values in array.
+    let dataFilePath=FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist") //this is will create your own datafile plist named Item.plist
+           
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]
-        {
-            itemArray = items
-        }
+        
+       loadItems()
+        //print(dataFilePath)
+//        let newItem=Item()
+//        newItem.title="kem cho"
+//        itemArray.append(newItem)
+//
+//        let newItem1=Item()
+//        newItem1.title="supp"
+//        itemArray.append(newItem1)
+//
+//        let newItem2=Item()
+//        newItem2.title="hola"
+//        itemArray.append(newItem2)
+//
+//        let newItem3=Item()
+//        newItem3.title="all good"
+        
+//       if let items = defaults.array(forKey: "ToDoListArray") as? [Item]        { //this should not be used since we can only store array,int and small values in "defaults" but we are storing an array of objects which will show error when we add any new item error:"Attempt to set non-property list object"
+//           itemArray = items
+//       }
         // Do any additional setup after loading the view.
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -18,21 +37,54 @@ class ToDoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text=itemArray[indexPath.row]
+        let item=itemArray[indexPath.row]
+        cell.textLabel?.text=item.title
+        
+        //ternary operator:
+        //value=condition ? valueOfTrue:valueOfFalse
+        cell.accessoryType = item.done ? .checkmark:.none
+        
+//        if item.done==true
+//        {
+//            cell.accessoryType  = .checkmark
+//        }
+//        else
+//        {
+//            cell.accessoryType  = .none
+//        }
+        
+        
+        
+        
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else
-        {
-
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done //this is same as below if else condition this means that it will set opposipte value ! means opposite
+        saveItems() //this is done so that when check then it will convert it into "YES" in "Item.plist"
+        
+//        if itemArray[indexPath.row].done == false //this is done so that tick mark is not repeated when we scroll down since this is ensure that after pressing tick it will convert bool func to true and so onn
+//        {
+//            itemArray[indexPath.row].done = true
+//        }
+//        else
+//        {
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        
+       // tableView.reloadData() //this is used so that after "done" is made true or false this will again call "cellforRow" func and make "checkmark" ie makes tick mark
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+//        {
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//        else
+//        {
+//
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -42,11 +94,15 @@ class ToDoListViewController: UITableViewController {
         
         let alert=UIAlertController(title: "Add new To Do List", message: "", preferredStyle: .alert)
         let action=UIAlertAction(title: "Add Item", style: .default) { (action) in
-           //textField.text
-            self.itemArray.append(textField.text!)
+           
+            let newItem=Item()
+            newItem.title=textField.text!
+            self.itemArray.append(newItem)
            // print(self.itemArray)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray") //used to store array values in defaults so if we access after app is reopened then will not removed anything
-            self.tableView.reloadData()
+           // self.defaults.set(self.itemArray, forKey: "ToDoListArray") //used to store array values in defaults so if we access after app is reopened then will not removed anything
+            
+            self.saveItems()
+            
             
             
         }
@@ -57,7 +113,33 @@ class ToDoListViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    func saveItems(){
+        let encoder=PropertyListEncoder()
+            do{
+        let data = try encoder.encode(itemArray)//this will encode ie make item array into plist(propertList)
+                try data.write(to:dataFilePath!) //this is used to write the data at location self.dataFilePath
+            }catch{
+                print("Error:\(error)")
+            }
+            
+        
+            tableView.reloadData()
+    }
+    func loadItems()
+    {
+        if let data=try? Data(contentsOf: dataFilePath!)
+        {
+            let decoder=PropertyListDecoder()
+            do{
+                itemArray=try decoder.decode([Item].self, from: data) //this will add new  data into itemarray and basically                                                 this will load up when apps loads up
+            }catch{
+                print("Error:\(error)")
+            }
+                                                       
+        }
+    }
 }
 
 
     
+
